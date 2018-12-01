cc0789f0a0e04cae9592c500af3db8b577c5b328
David Haney <david.haney@gmail.com>
2018-12-01 15:41:28 -0800

    Apply "black" formatting to python files

--- ansel/__init__.py
+++ ansel/__init__.py
@@ -3,5 +3,6 @@
 """Top-level package for ANSEL Codecs."""
 
 __author__ = """David Haney"""
-__email__ = 'david.haney@gmail.com'
-__version__ = '0.1.0'
+__email__ = "david.haney@gmail.com"
+__version__ = "0.1.0"
+
