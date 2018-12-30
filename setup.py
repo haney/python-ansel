8f01657840029c6d3a5ce66cfef6799ebb337ed7
David Haney <david.haney@gmail.com>
2018-12-30 13:50:04 -0800

    Fix incorrect repo reference

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
-    url="https://github.com/haney/ansel",
+    url="https://github.com/haney/python-ansel",
     version="0.1.0",
     zip_safe=True,
 )
